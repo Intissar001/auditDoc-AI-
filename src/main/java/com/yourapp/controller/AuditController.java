5dde2b33b7367d174c2d4f7dd75401eb3d0ad79f
Fatima Boudi <boudifatima450@gmail.com>
2025-12-13 01:45:51 +0100

    Fix: Add missing imports (IOException, FXMLLoader, BorderPane, HBox) in AuditController

--- src/main/java/com/yourapp/controller/AuditController.java
+++ src/main/java/com/yourapp/controller/AuditController.java
@@ -1,13 +1,17 @@
 package com.yourapp.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.ComboBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.TransferMode;
 import javafx.scene.input.Dragboard;
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
