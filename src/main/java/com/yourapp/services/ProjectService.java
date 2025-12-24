077dba4d11919a70979997eba53699cd9d7cfa96
lamyae AIT SI SAID <lamya.aitsisaid10@gmail.com>
2025-12-24 23:32:00 +0100

    amélioration du gestionnaire de documents
    # Conflicts:
    #	src/main/resources/application.yml

--- src/main/java/com/yourapp/services/ProjectService.java
+++ src/main/java/com/yourapp/services/ProjectService.java
@@ -1,5 +1,7 @@
 package com.yourapp.services;
 
+import com.yourapp.DAO.AuditDocumentRepository;
+import com.yourapp.model.AuditDocument;
 import com.yourapp.model.Project;
 import com.yourapp.DAO.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +14,21 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired private AuditDocumentRepository auditDocumentRepository;
+
     public List<Project> getAllProjects() {
         return projectRepository.findAll();
     }
 
     public void saveProject(Project project) {
         projectRepository.save(project);
+    }
+
+    public List<AuditDocument> getDocumentsByProjectId(Long projectId) {
+        return auditDocumentRepository.findByProjectId(projectId);
+    }
+
+    public void deleteDocument(AuditDocument doc) {
+        auditDocumentRepository.delete(doc);
     }
 }
